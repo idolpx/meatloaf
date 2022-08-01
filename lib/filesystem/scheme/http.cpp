c9fcb1dcceddd9a8fad10dc19c6aabfdd1c73b94
pszczygielski1 <qus123@gmail.com>
2022-08-01 15:43:47 +0200

    some more code

--- lib/filesystem/scheme/http.cpp
+++ lib/filesystem/scheme/http.cpp
@@ -3,6 +3,7 @@
 /********************************************************
  * File impls
  ********************************************************/
+// char local_response_buffer[MAX_HTTP_OUTPUT_BUFFER] = {0};
 
 bool HttpFile::isDirectory() {
     // hey, why not?
@@ -142,6 +143,8 @@ bool HttpOStream::open() {
         .timeout_ms = 10000,
         .disable_auto_redirect = false,
         .max_redirection_count = 10,
+        // .event_handler = _http_event_handler,
+        // .user_data = local_response_buffer,
         .keep_alive_enable = true,
         .keep_alive_idle = 10,
         .keep_alive_interval = 10,
@@ -326,3 +329,78 @@ size_t HttpIStream::read(uint8_t* buf, size_t size) {
 bool HttpIStream::isOpen() {
     return m_isOpen;
 };
+
+// esp_err_t _http_event_handler(esp_http_client_event_t *evt)
+// {
+//     static char *output_buffer;  // Buffer to store response of http request from event handler
+//     static int output_len;       // Stores number of bytes read
+//     switch(evt->event_id) {
+//         case HTTP_EVENT_ERROR:
+//             ESP_LOGD(TAG, "HTTP_EVENT_ERROR");
+//             break;
+//         case HTTP_EVENT_ON_CONNECTED:
+//             ESP_LOGD(TAG, "HTTP_EVENT_ON_CONNECTED");
+//             break;
+//         case HTTP_EVENT_HEADER_SENT:
+//             ESP_LOGD(TAG, "HTTP_EVENT_HEADER_SENT");
+//             break;
+//         case HTTP_EVENT_ON_HEADER:
+//             ESP_LOGD(TAG, "HTTP_EVENT_ON_HEADER, key=%s, value=%s", evt->header_key, evt->header_value);
+//             break;
+//         case HTTP_EVENT_ON_DATA:
+//             ESP_LOGD(TAG, "HTTP_EVENT_ON_DATA, len=%d", evt->data_len);
+//             /*
+//              *  Check for chunked encoding is added as the URL for chunked encoding used in this example returns binary data.
+//              *  However, event handler can also be used in case chunked encoding is used.
+//              */
+//             if (!esp_http_client_is_chunked_response(evt->client)) {
+//                 // If user_data buffer is configured, copy the response into the buffer
+//                 if (evt->user_data) {
+//                     memcpy(evt->user_data + output_len, evt->data, evt->data_len);
+//                 } else {
+//                     if (output_buffer == NULL) {
+//                         output_buffer = (char *) malloc(esp_http_client_get_content_length(evt->client));
+//                         output_len = 0;
+//                         if (output_buffer == NULL) {
+//                             ESP_LOGE(TAG, "Failed to allocate memory for output buffer");
+//                             return ESP_FAIL;
+//                         }
+//                     }
+//                     memcpy(output_buffer + output_len, evt->data, evt->data_len);
+//                 }
+//                 output_len += evt->data_len;
+//             }
+
+//             break;
+//         case HTTP_EVENT_ON_FINISH:
+//             ESP_LOGD(TAG, "HTTP_EVENT_ON_FINISH");
+//             if (output_buffer != NULL) {
+//                 // Response is accumulated in output_buffer. Uncomment the below line to print the accumulated response
+//                 // ESP_LOG_BUFFER_HEX(TAG, output_buffer, output_len);
+//                 free(output_buffer);
+//                 output_buffer = NULL;
+//             }
+//             output_len = 0;
+//             break;
+//         case HTTP_EVENT_DISCONNECTED:
+//             ESP_LOGI(TAG, "HTTP_EVENT_DISCONNECTED");
+//             int mbedtls_err = 0;
+//             esp_err_t err = esp_tls_get_and_clear_last_error((esp_tls_error_handle_t)evt->data, &mbedtls_err, NULL);
+//             if (err != 0) {
+//                 ESP_LOGI(TAG, "Last esp error code: 0x%x", err);
+//                 ESP_LOGI(TAG, "Last mbedtls failure: 0x%x", mbedtls_err);
+//             }
+//             if (output_buffer != NULL) {
+//                 free(output_buffer);
+//                 output_buffer = NULL;
+//             }
+//             output_len = 0;
+//             break;
+//         case HTTP_EVENT_REDIRECT:
+//             ESP_LOGD(TAG, "HTTP_EVENT_REDIRECT");
+//             esp_http_client_set_header(evt->client, "From", "user@example.com");
+//             esp_http_client_set_header(evt->client, "Accept", "text/html");
+//             break;
+//     }
+//     return ESP_OK;
+// }
