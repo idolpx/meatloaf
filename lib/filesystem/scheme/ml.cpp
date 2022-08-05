0ca47b23e400ed65b9980ff6d5af42cbc168eb35
idolpx <jaime@idolpx.com>
2022-08-05 17:51:29 -0400

    Yes! ML: working with new http

--- lib/filesystem/scheme/ml.cpp
+++ lib/filesystem/scheme/ml.cpp
@@ -20,29 +20,6 @@ bool MLIStream::open() {
     std::string ml_url = "https://api.meatloaf.cc/?" + urlParser.name;
     url = ml_url;
     
-    // a new way to do that:
-    // 1. put an instance of MeatHttpClient somewhere in this class as a class field
-    MeatHttpClient client;
-    // 2. if you need to process some headers, use this lambda:
-    client.setOnHeader([this](char* header, char* value) {
-        // did we receive ml_media_header?
-        if(strcmp("ml_media_header", header)==0) {
-            // assign this header value to something
-            this->url = value;
-        }
-        if(strcmp("ml_media_header", header)==0) {
-            // assign this header value to something
-            this->url = value;
-        }
-        if(strcmp("ml_media_header", header)==0) {
-            // assign this header value to something
-            this->url = value;
-        }
-        if(strcmp("ml_media_header", header)==0) {
-            // assign this header value to something
-            this->url = value;
-        }
-        return ESP_OK;
-    });
-    return client.GET(url);
+    Debug_printv("url[%s]", url.c_str());
+    return m_http.GET(url);
 };
