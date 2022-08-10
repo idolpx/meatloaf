3933b4e6c0dc6b6f0da4832b4df4e44ddcf8187d
mozzwald <mozzwald@gmail.com>
2022-08-10 15:15:56 -0500

    Add fujiapple-rev0 pinmap

--- include/pinmap.h
+++ include/pinmap.h
@@ -3,8 +3,9 @@
 #define PINMAP_H
 
 //#include "pinmap/fujinet-v1.6.h"
-#include "pinmap/lolin-d32-pro.h"
+//#include "pinmap/lolin-d32-pro.h"
 //#include "pinmap/esp32-a1s-audio-kit-v2.2.h"
 //#include "pinmap/esp32s3.h"
+#include "pinmap/fujiapple-rev0.h"
 
 #endif
