888802821d8988e2e4c73d6803dc93560d278071
idolpx <jaime@idolpx.com>
2022-09-19 18:57:53 -0400

    Add Lolin S2 Mini pinmap

--- include/pinmap.h
+++ include/pinmap.h
@@ -4,6 +4,7 @@
 
 //#include "pinmap/fujinet-v1.6.h"
 #include "pinmap/lolin-d32-pro.h"
+//#include "pinmap/lolin-s2-mini.h"
 //#include "pinmap/esp-wroom-32.h"
 //#include "pinmap/esp32-a1s-audio-kit-v2.2.h"
 //#include "pinmap/esp32s3.h"
