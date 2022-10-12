d73bf88fdacb11c7f952ca2c3f3d08c2f76361d9
idolpx <jaime@idolpx.com>
2022-10-12 16:46:54 -0400

    FN 1.6 pinmap

--- include/pinmap/fujinet-v1.6.h
+++ include/pinmap/fujinet-v1.6.h
@@ -1,6 +1,6 @@
 /* FujiNet Hardware Pin Mapping */
-#ifndef PINMAP_CBM_LOLIN_D32_PRO_H
-#define PINMAP_CBM_LOLIN_D32_PRO_H
+#ifndef PINMAP_FUJINET_V1_6_H
+#define PINMAP_FUJINET_V1_6_H
 
 /* SD Card */
 // pins 12-15 are used to interface with the JTAG debugger
@@ -9,18 +9,11 @@
 #define PIN_CARD_DETECT 12 // fnSystem.h
 #define PIN_CARD_DETECT_FIX 15 // fnSystem.h
 #endif
-#ifdef CONFIG_IDF_TARGET_ESP32S3
-#define PIN_SD_HOST_MISO 19
-#define PIN_SD_HOST_MOSI 23
-#define PIN_SD_HOST_SCK  18
-#define PIN_SD_HOST_CS   GPIO_NUM_5
-#else
-//#define PIN_SD_HOST_CS GPIO_NUM_5 // fnFsSD.cpp
-#define PIN_SD_HOST_CS GPIO_NUM_4 // LOLIN D32 Pro
+
+#define PIN_SD_HOST_CS GPIO_NUM_5 // fnFsSD.cpp
 #define PIN_SD_HOST_MISO GPIO_NUM_19
 #define PIN_SD_HOST_MOSI GPIO_NUM_23
 #define PIN_SD_HOST_SCK GPIO_NUM_18
-#endif
 
 /* UART */
 #define PIN_UART0_RX 3 // fnUART.cpp
@@ -59,34 +52,26 @@
 // Reset line is available
 #define IEC_HAS_RESET
 
-// // CBM IEC Serial Port
-// #define PIN_IEC_ATN         GPIO_NUM_39      // SIO 7  - CMD  - Command
-// #define PIN_IEC_SRQ			GPIO_NUM_26      // SIO 13 - INT  - Interrupt
-// #define PIN_IEC_RESET       GPIO_NUM_22      // SIO 9  - PROC - Proceed
-//                                              // SIO 4 & 6 - GND - Ground
+// CBM IEC Serial Port
+#define PIN_IEC_ATN         GPIO_NUM_39      // SIO 7  - CMD  - Command
+#define PIN_IEC_SRQ			GPIO_NUM_26      // SIO 13 - INT  - Interrupt
+#define PIN_IEC_RESET       GPIO_NUM_22      // SIO 9  - PROC - Proceed
+                                             // SIO 4 & 6 - GND - Ground
 
-// // IEC_SPLIT_LINES
-// #ifndef IEC_SPLIT_LINES
-// // NOT SPLIT - Bidirectional Lines
-// #define PIN_IEC_CLK_IN		GPIO_NUM_27      // SIO 1  - CKI  - Clock Input
-// #define PIN_IEC_CLK_OUT	    GPIO_NUM_27      // SIO 1  - CKI  - Clock Input
-// #define PIN_IEC_DATA_IN    	GPIO_NUM_21      // SIO 3  - DI   - Data Input
-// #define PIN_IEC_DATA_OUT   	GPIO_NUM_21      // SIO 3  - DI   - Data Input
-// #else
-// // SPLIT - Seperate Input & Output lines
-// #define PIN_IEC_CLK_IN		GPIO_NUM_32      // SIO 2  - CKO  - Clock Output
-// #define PIN_IEC_CLK_OUT		GPIO_NUM_27      // SIO 1  - CKI  - Clock Input
-// #define PIN_IEC_DATA_IN     GPIO_NUM_33      // SIO 5  - DO   - Data Output
-// #define PIN_IEC_DATA_OUT    GPIO_NUM_21      // SIO 3  - DI   - Data Input
-// #endif
+// IEC_SPLIT_LINES
+#ifndef IEC_SPLIT_LINES
+// NOT SPLIT - Bidirectional Lines
+#define PIN_IEC_CLK_IN		GPIO_NUM_27      // SIO 1  - CKI  - Clock Input
+#define PIN_IEC_CLK_OUT	    GPIO_NUM_27      // SIO 1  - CKI  - Clock Input
+#define PIN_IEC_DATA_IN    	GPIO_NUM_21      // SIO 3  - DI   - Data Input
+#define PIN_IEC_DATA_OUT   	GPIO_NUM_21      // SIO 3  - DI   - Data Input
+#else
+// SPLIT - Seperate Input & Output lines
+#define PIN_IEC_CLK_IN		GPIO_NUM_32      // SIO 2  - CKO  - Clock Output
+#define PIN_IEC_CLK_OUT		GPIO_NUM_27      // SIO 1  - CKI  - Clock Input
+#define PIN_IEC_DATA_IN     GPIO_NUM_33      // SIO 5  - DO   - Data Output
+#define PIN_IEC_DATA_OUT    GPIO_NUM_21      // SIO 3  - DI   - Data Input
+#endif
 
-// LOLIN D32 PRO 
-#define PIN_IEC_RESET       GPIO_NUM_34
-#define PIN_IEC_ATN         GPIO_NUM_32
-#define PIN_IEC_CLK_IN		GPIO_NUM_33
-#define PIN_IEC_CLK_OUT	    GPIO_NUM_33
-#define PIN_IEC_DATA_IN    	GPIO_NUM_25
-#define PIN_IEC_DATA_OUT   	GPIO_NUM_25
-#define PIN_IEC_SRQ			GPIO_NUM_26
 
-#endif // PINMAP_CBM_LOLIN_D32_PRO_H
+#endif // PINMAP_FUJINET_V1_6_H
