02b461b9815b291a560044093cd7d9addd1a399e
mozzwald <mozzwald@gmail.com>
2022-11-12 11:31:28 -0600

    Don't enable old LED code if using fujiloaf board

--- lib/hardware/led.cpp
+++ lib/hardware/led.cpp
@@ -1,4 +1,3 @@
-
 #include "led.h"
 
 #include "fnSystem.h"
@@ -20,6 +19,7 @@ LedManager::LedManager()
 // Sets required pins to OUTPUT mode and makes sure they're initially off
 void LedManager::setup()
 {
+#ifndef FUJILOAF_REV0
     fnSystem.set_pin_mode(PIN_LED_BUS, gpio_mode_t::GPIO_MODE_OUTPUT);
     fnSystem.digital_write(PIN_LED_BUS, DIGI_HIGH);
  
@@ -28,21 +28,27 @@ void LedManager::setup()
 
     fnSystem.set_pin_mode(PIN_LED_WIFI, gpio_mode_t::GPIO_MODE_OUTPUT);
     fnSystem.digital_write(PIN_LED_WIFI, DIGI_HIGH);
+#endif
 }
 
 void LedManager::set(eLed led, bool on)
 {
+#ifndef FUJILOAF_REV0
     mLedState[led] = on;
     fnSystem.digital_write(mLedPin[led], (on ? DIGI_LOW : DIGI_HIGH));
+#endif
 }
 
 void LedManager::toggle(eLed led)
 {
+#ifndef FUJILOAF_REV0
     set(led, !mLedState[led]);
+#endif
 }
 
 void LedManager::blink(eLed led, int count)
 {
+#ifndef FUJILOAF_REV0
     for(int i = 0; i < count; i++)
     {
         toggle(led);
@@ -51,4 +57,5 @@ void LedManager::blink(eLed led, int count)
         if(i < count - 1)
             fnSystem.delay(BLINKING_TIME);
     }
+#endif
 }
