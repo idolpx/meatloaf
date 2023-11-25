ba0a9e281be9459bfc4bc33f15eb4f5b84239128
idolpx <jaime@idolpx.com>
2023-11-25 10:38:36 -0600

    remove unecessary check

--- lib/bus/iec/protocol/iecProtocolSerial.cpp
+++ lib/bus/iec/protocol/iecProtocolSerial.cpp
@@ -121,8 +121,8 @@ bool IecProtocolSerial::sendByte(uint8_t data, bool eoi)
     // happened. If EOI was sent or received in this last transmission, both talker and listener "letgo."  After a suitable pause,
     // the Clock and Data lines are RELEASED to false and transmission stops.
 
-    if ( eoi )
-        IEC.release ( PIN_IEC_CLK_OUT );
+    // if ( eoi )
+    //     IEC.release ( PIN_IEC_CLK_OUT );
 
     timeoutWait( PIN_IEC_DATA_IN, RELEASED, TIMING_Tbb);
 
@@ -160,12 +160,12 @@ bool IecProtocolSerial::sendBits ( uint8_t data )
     }
 
     IEC.release(PIN_IEC_DATA_OUT);
-    // Check to see if DATA is being pulled
-    if ( IEC.status( PIN_IEC_DATA_IN ) == PULLED )
-    {
-        Debug_printv(".");
-        return false; // If it is we exit
-    }
+    // // Check to see if DATA is being pulled
+    // if ( IEC.status( PIN_IEC_DATA_IN ) == PULLED )
+    // {
+    //     Debug_printv(".");
+    //     return false; // If it is we exit
+    // }
 
     // Send bits
     for ( uint8_t n = 0; n < 8; n++ )
