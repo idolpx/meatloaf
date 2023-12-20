6ee40b61643499f2f5d268bb00b914aa27d41ea9
idolpx <jaime@idolpx.com>
2023-12-19 18:42:59 -0600

    add test_meatloaf

---
+++ test/native/test_meatloaf/test_meatloaf.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+#include "unity.h"
+
+#include <dirent.h>
+#include <sys/stat.h>
+
+// #include "meat_io.h"
+// #include "meat_stream.h"
+// #include "meat_buffer.h"
+
+void setUp(void)
+{
+}
+
+void tearDown(void)
+{
+}
+
+void test_meatloaf_mfile_directory(void)
+{
+    std::string path = ".";
+
+    DIR *dir;
+    struct dirent *ent;
+    struct stat st;
+
+    if ((dir = opendir ( path.c_str() )) != NULL) {
+        /* print all the files and directories within directory */
+        while ((ent = readdir (dir)) != NULL) {
+            stat(ent->d_name, &st);
+            printf ("%lld %s %hu\r\n", st.st_size, ent->d_name, st.st_mode);
+        }
+        closedir (dir);
+    } else {
+        /* could not open directory */
+        printf ("error");
+    }
+}
+
+
+void process()
+{
+    UNITY_BEGIN();
+
+    RUN_TEST(test_meatloaf_mfile_directory);
+
+    UNITY_END();
+}
+
+int main(int argc, char **argv)
+{
+    process();
+}
