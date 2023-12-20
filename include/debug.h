d8d497977860e3e09cef74324c216b9b9b23791c
idolpx <jaime@idolpx.com>
2023-12-19 23:27:56 -0600

    more unit testing mods

--- include/debug.h
+++ include/debug.h
@@ -14,28 +14,32 @@
 #undef DEBUG
 #endif
 
+#ifdef TEST_NATIVE
 #include "../lib/hardware/fnUART.h"
 #define Serial fnUartDebug
+#endif
 
 /*
   Debugging Macros
 */
 #ifdef DEBUG
-    // Use FujiNet debug serial output
+#ifdef TEST_NATIVE
+    #define Debug_print(...) print( __VA_ARGS__ )
+    #define Debug_printf(...) printf( __VA_ARGS__ )
+    #define Debug_println(...) println( __VA_ARGS__ )
+    #define Debug_printv(format, ...) {printf( ANSI_YELLOW "[%s:%u] %s(): " ANSI_GREEN_BOLD format ANSI_RESET "\r\n", __FILE__, __LINE__, __FUNCTION__, ##__VA_ARGS__);}
+#else
     #define Debug_print(...) fnUartDebug.print( __VA_ARGS__ )
     #define Debug_printf(...) fnUartDebug.printf( __VA_ARGS__ )
     #define Debug_println(...) fnUartDebug.println( __VA_ARGS__ )
     #define Debug_printv(format, ...) {fnUartDebug.printf( ANSI_YELLOW "[%s:%u] %s(): " ANSI_GREEN_BOLD format ANSI_RESET "\r\n", __FILE__, __LINE__, __FUNCTION__, ##__VA_ARGS__);}
-
-    #define HEAP_CHECK(x) Debug_printf("HEAP CHECK %s " x "\r\n", heap_caps_check_integrity_all(true) ? "PASSED":"FAILED")
 #endif
-
-#ifndef DEBUG
+    #define HEAP_CHECK(x) Debug_printf("HEAP CHECK %s " x "\r\n", heap_caps_check_integrity_all(true) ? "PASSED":"FAILED")
+#else
     #define Debug_print(...)
     #define Debug_printf(...)
     #define Debug_println(...)
     #define Debug_printv(format, ...)
-
     #define HEAP_CHECK(x)
 #endif
 
--- test/native/test_meatloaf/test_meatloaf.cpp
+++ test/native/test_meatloaf/test_meatloaf.cpp
@@ -4,7 +4,7 @@
 #include <dirent.h>
 #include <sys/stat.h>
 
-// #include "meat_io.h"
+#include "meat_io.h"
 // #include "meat_stream.h"
 // #include "meat_buffer.h"
 
@@ -37,12 +37,36 @@ void test_meatloaf_mfile_directory(void)
     }
 }
 
+void test_meatloaf_mfile_properties(void)
+{
+    std::unique_ptr<MFile> testMFile = Meat::New<MFile>("http://c64.meatloaf.cc/fb64");
+
+    printf("\n\n* %s File properties\r\n", testMFile->url.c_str());
+    printf("Url: %s, isDir = %d\r\n", testMFile->url.c_str(), testMFile->isDirectory());
+    printf("Scheme: [%s]\r\n", testMFile->scheme.c_str());
+    printf("Username: [%s]\r\n", testMFile->user.c_str());
+    printf("Password: [%s]\r\n", testMFile->pass.c_str());
+    printf("Host: [%s]\r\n", testMFile->host.c_str());
+    printf("Port: [%s]\r\n", testMFile->port.c_str());    
+    printf("Path: [%s]\r\n", testMFile->path.c_str());
+
+    if ( testMFile->streamFile )
+        printf("stream src: [%s]\r\n", testMFile->streamFile->url.c_str());
+
+    printf("path in stream: [%s]\r\n", testMFile->pathInStream.c_str());
+    printf("File: [%s]\r\n", testMFile->name.c_str());
+    printf("Extension: [%s]\r\n", testMFile->extension.c_str());
+    printf("Size: [%d]\r\n", testMFile->size());
+    printf("Is text: [%d]\r\n", testMFile->isText());
+    printf("-------------------------------\r\n");
+}
 
 void process()
 {
     UNITY_BEGIN();
 
     RUN_TEST(test_meatloaf_mfile_directory);
+    RUN_TEST(test_meatloaf_mfile_properties);
 
     UNITY_END();
 }
