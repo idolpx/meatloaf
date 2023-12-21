bc705699cfe16e1c70dcc68229c468dc55380a6c
Przemyslaw Szczygielski <przemyslaw.szczygielski@loyaltek.com>
2023-12-21 18:21:32 +0100

    Move tests again

--- test/native/test_strings/test_stringops.cpp
+++ test/native/test_strings/test_stringops.cpp
@@ -1,7 +1,8 @@
 #include "unity.h"
 
 #include "punycode.h"
-
+#include <string>
+// #include "../lib/utils/string_utils.cpp"
 
 void setUp(void)
 {
@@ -17,7 +18,7 @@ void test_PetsciiUtf() {
     std::string petchar = "\xb0";
     std::string utf8char = mstr::toUTF8(petchar);
     // print hex representation of utf8char
-    Debug_printv("Petscii b0 as UTF8: %x %x %x (should be e2 94 8c)\r\n", utf8char[0],utf8char[1],utf8char[2]);
+    // Debug_printv("Petscii b0 as UTF8: %x %x %x (should be e2 94 8c)\r\n", utf8char[0],utf8char[1],utf8char[2]);
 
 
     // should give:                        0x250c,0x2534,0x252c,0x2524,
@@ -26,10 +27,10 @@ void test_PetsciiUtf() {
     std::string utf8 = mstr::toUTF8(petscii);
     std::string petscii2 = mstr::toPETSCII2(utf8);
     std::string utf8again = mstr::toUTF8(petscii2);
-    Debug_printv("Petscii: [%s]\r\n", petscii.c_str());
-    Debug_printv("UTF8: [%s] - should be: [\u250c\u2534\u252c\u2524]\r\n", utf8.c_str());
-    Debug_printv("And back to petscii: [%s]\r\n", petscii2.c_str());
-    Debug_printv("And back to utf8: [%s]\r\n", utf8again.c_str());
+    // Debug_printv("Petscii: [%s]\r\n", petscii.c_str());
+    // Debug_printv("UTF8: [%s] - should be: [\u250c\u2534\u252c\u2524]\r\n", utf8.c_str());
+    // Debug_printv("And back to petscii: [%s]\r\n", petscii2.c_str());
+    // Debug_printv("And back to utf8: [%s]\r\n", utf8again.c_str());
 }
 
 void test_Punycode() {
@@ -44,8 +45,8 @@ void test_Punycode() {
     // size_t punycode_encode(const uint32_t *const src, const size_t srclen, char *const dst, size_t *const dstlen)
     punycode_encode(chineseAsUnicode, 5, asPunycode, &dstlen);
     std::string punycode(asPunycode, dstlen);
-    Debug_printv("Chinese U32 as punycode:'%s'\n", punycode.c_str());
-    std::string asUnicode = U8Char::fromPunycode(punycode);
+    // Debug_printv("Chinese U32 as punycode:'%s'\n", punycode.c_str());
+    // std::string asUnicode = U8Char::fromPunycode(punycode);
     // Debug_printv("Chinese UTF8 from the above punycode:'%s'\n", asUnicode.c_str());
 
 //     std::string punycode2 = U8Char::toPunycode(asUnicode);
