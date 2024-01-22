e72080386d848b47d3e04a073854e10f1fa90233
Henrik Olsson <henrik.olsson@bokusgruppen.com>
2024-01-22 16:15:09 +0100

    Working psram

--- src/main.cpp
+++ src/main.cpp
@@ -70,9 +70,7 @@ void main_setup()
 
 #ifdef BOARD_HAS_PSRAM
     Serial.printf( "PsramSize %u\r\n", fnSystem.get_psram_size() );
-#ifdef CONFIG_IDF_TARGET_ESP32S3
-    Serial.printf( "Psram size %u\r\n", esp_psram_get_size() );
-#else
+#if !defined(CONFIG_IDF_TARGET_ESP32S3)
     Serial.printf( "himem phys %u\r\n", esp_himem_get_phys_size() );
     Serial.printf( "himem free %u\r\n", esp_himem_get_free_size() );
     Serial.printf( "himem reserved %u\r\n", esp_himem_reserved_area_size() );
