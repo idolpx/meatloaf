c46bccf4fc99cf9f27106469413310bf1ac1ea56
Jaime Idolpx <jaime@idolpx.com>
2024-06-24 15:59:35 -0400

    remove old mklittlefs hack

--- build_version.py
+++ build_version.py
@@ -9,14 +9,6 @@ Import("env")
 #env.Execute("$PYTHONEXE -m pip list --format=json --disable-pip-version-check");
 #exit(0);
 
-# Don't do anything if this is an 'uploadfs' or 'erase' target
-cmdline = ','.join(sys.argv)
-if cmdline.find('buildfs') or cmdline.find('uploadfs'):
-    # Change build tool if we are using LittleFS
-    if any("FLASH_LITTLEFS" in x for x in env['BUILD_FLAGS']):
-        print("\033[1;31mReplacing MKSPIFFSTOOL with mklittlefs\033[1;37m")
-        env.Replace (MKSPIFFSTOOL = "mklittlefs")
-
 # Disable automatic versioning
 if 1:
     print("Automatic versioning disabled")
