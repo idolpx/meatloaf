dbb691f8715b58b525bd056c92afca0dabad915d
Jaime Idolpx <jaime@idolpx.com>
2025-11-26 02:08:01 -0600

    fill in gpio console commands

--- lib/console/Commands/GPIOCommands.cpp
+++ lib/console/Commands/GPIOCommands.cpp
@@ -5,156 +5,137 @@
 #include <stdexcept>
 
 #include "string_utils.h"
+#include "esp-idf-arduino.h"
 
 #define HIGH 1
 #define LOW  0
 
 static int _pinmode(int argc, char **argv)
 {
-    // if (argc != 3)
-    // {
-    //     Serial.printf("You have to pass a pin number and mode. Syntax: pinMode [GPIO] [MODE]\r\n");
-    //     return 1;
-    // }
+    if (argc != 3)
+    {
+        printf("You have to pass a pin number and mode. Syntax: pinMode [GPIO] [MODE]\r\n");
+        return 1;
+    }
 
-    // char *pin_str = argv[1];
-    // std::string mode_str = std::string(argv[2]);
+    char *pin_str = argv[1];
+    std::string mode_str = std::string(argv[2]);
 
-    // unsigned long pin = 0;
+    unsigned long pin = 0;
 
-    // try
-    // {
-    //     pin = std::stoul(pin_str);
-    // }
-    // catch (std::invalid_argument ex)
-    // {
-    //     Serial.Serial.printf("Invalid argument for pin: %s\r\n", ex.what());
-    //     return 1;
-    // }
+    pin = std::stoul(pin_str);
 
-    // if (pin > 255 || !digitalPinIsValid(pin)) {
-    //     Serial.Serial.printf("%d is not a GPIO pin\r\n", pin);
-    //     return 1;
-    // }
+    if (pin > GPIO_NUM_MAX) {
+        printf("%ld is not a GPIO pin\r\n", pin);
+        return 1;
+    }
 
-    // gpio_mode_t mode = GPIO_MODE_INPUT;
+    gpio_mode_t mode = GPIO_MODE_INPUT;
 
-    // if (mstr::equals(mode_str, "INPUT", false))
-    // {
-    //     mode = GPIO_MODE_INPUT;
-    // }
-    // else if (mstr::equals(mode_str, "OUTPUT"))
-    // {
-    //     mode = GPIO_MODE_OUTPUT;
-    // }
-    // else if (mstr::equals(mode_str, "INPUT_PULLUP"))
-    // {
-    //     mode = GPIO_MODE_INPUT | GPIO_PULLUP_ENABLE;
-    // }
-    // else if (mstr::equals(mode_str, "INPUT_PULLDOWN"))
-    // {
-    //     mode = GPIO_MODE_INPUT | GPIO_PULLDOWN_ENABLE;
-    // }
-    // else if (mstr::equals(mode_str, "OUTPUT_OPEN_DRAIN"))
-    // {
-    //     mode = GPIO_MODE_OUTPUT_OD;
-    // }
-    // else
-    // {
-    //     Serial.printf("Invalid mode: Allowed modes are INPUT, OUTPUT, INPUT_PULLUP, INPUT_PULLDOWN, OUTPUT_OPEN_DRAIN\r\n");
-    // }
+    if (mstr::equals(mode_str, "INPUT", false))
+    {
+        mode = GPIO_MODE_INPUT;
+    }
+    else if (mstr::equals(mode_str, "OUTPUT"))
+    {
+        mode = GPIO_MODE_OUTPUT;
+    }
+    else if (mstr::equals(mode_str, "INPUT_PULLUP"))
+    {
+        mode = (gpio_mode_t)(GPIO_MODE_INPUT | GPIO_PULLUP_ENABLE);
+    }
+    else if (mstr::equals(mode_str, "INPUT_PULLDOWN"))
+    {
+        mode = (gpio_mode_t)(GPIO_MODE_INPUT | GPIO_PULLDOWN_ENABLE);
+    }
+    else if (mstr::equals(mode_str, "OUTPUT_OPEN_DRAIN"))
+    {
+        mode = GPIO_MODE_OUTPUT_OD;
+    }
+    else
+    {
+        printf("Invalid mode: Allowed modes are INPUT, OUTPUT, INPUT_PULLUP, INPUT_PULLDOWN, OUTPUT_OPEN_DRAIN\r\n");
+    }
 
-    // pinMode(pin, mode);
-    // Serial.printf("Mode set successful.\r\n");
+    pinMode(pin, mode);
+    printf("Mode set successful.\r\n");
 
     return 0;
 }
 
 static int _digitalWrite(int argc, char** argv)
 {
-    // if (argc != 3)
-    // {
-    //     Serial.printf("You have to pass an pin number and level. Syntax: digitalWrite [GPIO] [Level]\r\n");
-    //     return 1;
-    // }
+    if (argc != 3)
+    {
+        printf("You have to pass an pin number and level. Syntax: digitalWrite [GPIO] [Level]\r\n");
+        return 1;
+    }
 
-    // char *pin_str = argv[1];
-    // std::string mode_str = std::string(argv[2]);
+    char *pin_str = argv[1];
+    std::string mode_str = std::string(argv[2]);
 
-    // unsigned long pin = 0;
+    unsigned long pin = 0;
 
-    // try
-    // {
-    //     pin = std::stoul(pin_str);
-    // }
-    // catch (std::invalid_argument ex)
-    // {
-    //     Serial.Serial.printf("Invalid argument for pin: %s\r\n", ex.what());
-    //     return 1;
-    // }
+    pin = std::stoul(pin_str);
 
-    // if (pin > 255 || !digitalPinCanOutput(pin)) {
-    //     Serial.Serial.printf("%d is not a GPIO pin\r\n", pin);
-    //     return 1;
-    // }
+    if (pin > GPIO_NUM_MAX) {
+        printf("%ld is not a GPIO pin\r\n", pin);
+        return 1;
+    }
 
-    // int mode = LOW;
+    int mode = LOW;
 
-    // if (mstr::equals(mode_str, "HIGH")  || mstr::equals(mode_str, "1"))
-    // {
-    //     mode = HIGH;
-    // }
-    // else if (mstr::equals(mode_str, "LOW") || mstr::equals(mode_str, "0"))
-    // {
-    //     mode = LOW;
-    // } else
-    // {
-    //     Serial.Serial.printf("Invalid mode: Allowed levels are HIGH, LOW, 0 and 1\r\n");
-    // }
+    if (mstr::equals(mode_str, "HIGH")  || mstr::equals(mode_str, "1"))
+    {
+        mode = HIGH;
+    }
+    else if (mstr::equals(mode_str, "LOW") || mstr::equals(mode_str, "0"))
+    {
+        mode = LOW;
+    } else
+    {
+        printf("Invalid mode: Allowed levels are HIGH, LOW, 0 and 1\r\n");
+    }
 
-    // pinMode(pin, mode);
-    // Serial.printf("Output set successful.\r\n");
+    int ret = gpio_set_level((gpio_num_t)pin, mode);
+    if (ret != ESP_OK) {
+        printf("Error setting pin level: %d\r\n", ret);
+        return ret;
+    }
+    printf("Output set successful.\r\n");
 
-    return 0;
+    return ret;
 }
 
 static int _digitalRead(int argc, char** argv)
 {
-    // if (argc != 2)
-    // {
-    //     Serial.printf("You have to pass an pin number to read\r\n");
-    //     return 1;
-    // }
+    if (argc != 2)
+    {
+        printf("You have to pass an pin number to read\r\n");
+        return 1;
+    }
 
-    // char *pin_str = argv[1];
+    char *pin_str = argv[1];
 
-    // unsigned long pin = 0;
+    unsigned long pin = 0;
 
-    // try
-    // {
-    //     pin = std::stoul(pin_str);
-    // }
-    // catch (std::invalid_argument ex)
-    // {
-    //     Serial.Serial.printf("Invalid argument for pin: %s\r\n", ex.what());
-    //     return 1;
-    // }
+    pin = std::stoul(pin_str);
 
-    // if (pin > 255 || !digitalPinCanOutput(pin)) {
-    //     Serial.Serial.printf("%d is not a GPIO pin\r\n", pin);
-    //     return 1;
-    // }
+    if (pin > GPIO_NUM_MAX) {
+        printf("%ld is not a GPIO pin\r\n", pin);
+        return 1;
+    }
 
-    // auto level = digitalRead(pin);
+    auto level = gpio_get_level((gpio_num_t)pin);
 
-    // if(level == HIGH) {
-    //     Serial.printf("HIGH\r\n");
-    // } else if(level == LOW) {
-    //     Serial.printf("LOW\r\n");
-    // } else {
-    //     Serial.Serial.printf("Unknown state (%u) of pin %u!\r\n", level, pin);
-    //     return 1;
-    // }
+    if(level == HIGH) {
+        printf("HIGH\r\n");
+    } else if(level == LOW) {
+        printf("LOW\r\n");
+    } else {
+        printf("Unknown state (%u) of pin %lu!\r\n", level, pin);
+        return 1;
+    }
 
     return 0;
 }
@@ -177,12 +158,12 @@ static int _analogRead(int argc, char** argv)
     // }
     // catch (std::invalid_argument ex)
     // {
-    //     Serial.Serial.printf("Invalid argument for pin: %s\r\n", ex.what());
+    //     printf("Invalid argument for pin: %s\r\n", ex.what());
     //     return 1;
     // }
 
     // if (pin > 255 || digitalPinToAnalogChannel(pin) == -1) {
-    //     Serial.Serial.printf("%d is not a ADC pin\r\n", pin);
+    //     printf("%d is not a ADC pin\r\n", pin);
     //     return 1;
     // }
 
