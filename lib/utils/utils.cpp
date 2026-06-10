e549903c200d3c2c625611883a8c0cb57bc79471
Jaime Idolpx <jaime@idolpx.com>
2026-06-10 18:32:05 -0400

    feat: Update version information and enable external memory for stack in SDK configuration

--- lib/utils/utils.cpp
+++ lib/utils/utils.cpp
@@ -618,9 +618,9 @@ void util_dump_bytes(const uint8_t *buff, uint32_t buff_size)
     {
         for (int k = 0; (k + j) < buff_size && k < bytes_per_line; k++)
             Debug_printf("%02X ", buff[k + j]);
-        Debug_println("\r\n");
+        Debug_println("");
     }
-    Debug_println("\r\n");
+    Debug_println("");
 }
 
 vector<string> util_tokenize(string s, char c)
