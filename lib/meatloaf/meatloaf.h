a3daa7a24c521a814815ab7645490d45c0cabfa9
Jaime Idolpx <jaime@idolpx.com>
2026-07-01 12:42:16 -0400

    refactor: adjust indentation for clarity in IECBusHandler.h and add debug print in seekPath method

--- lib/meatloaf/meatloaf.h
+++ lib/meatloaf/meatloaf.h
@@ -172,6 +172,7 @@ public:
     // For files with a browsable random access directory structure
     // d64, d74, d81, dnp, etc.
     virtual bool seekPath(std::string path) {
+        Debug_printv( "path[%s]", path.c_str() );
         return false;
     };
 
