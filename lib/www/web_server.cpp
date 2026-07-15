8373c44c5615b7e580b015ca6cea955546faa174
Jaime Idolpx <jaime@idolpx.com>
2026-07-15 03:20:04 -0400

    guard MIN/MAX to eliminate warning

--- lib/www/web_server.cpp
+++ lib/www/web_server.cpp
@@ -59,16 +59,18 @@ static inline void *psram_malloc(size_t sz)
 #include "fnConfig.h"
 #include "fnWiFi.h"
 
-
+#ifndef MIN
 #define MIN(a, b) \
     ({ __typeof__ (a) _a = (a); \
        __typeof__ (b) _b = (b); \
      _a < _b ? _a : _b; })
-
+#endif
+#ifndef MAX
 #define MAX(a, b) \
     ({ __typeof__ (a) _a = (a); \
        __typeof__ (b) _b = (b); \
      _a > _b ? _a : _b; })
+#endif
 
 HttpServer httpServer;
 
