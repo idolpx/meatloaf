a9d0915c3412b03b8d199d72aeddb43bdab0f727
Jaime Idolpx <jaime@idolpx.com>
2026-07-25 09:06:05 -0400

    Add files via upload

---
+++ include/version.h
@@ -0,0 +1,23 @@
+/*
+ This file is automatically modified by the build_version.py script
+ to update the BUILD and BUILD_DATE values based on the
+ current Git commit ID and current UTC date/time.
+
+ FN_VERSION_MAJOR and FN_VERSION_MINOR should be manually updated
+ as needed.
+*/
+
+#ifndef _VERSION_H
+#define _VERSION_H
+
+#define FN_VERSION_MAJOR 1
+#define FN_VERSION_MINOR 4
+
+#define FN_VERSION_BUILD "535e25cd"
+
+#define FN_VERSION_DATE "2026-07-25 07:42:44"
+
+#define FN_VERSION_FULL "1.4.535e25cd"
+
+#define FW_VERSION "20260725.07"
+#endif /* _VERSION_H */
